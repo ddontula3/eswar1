6c8069fa5b359b1ebfff65fcd922e775acd0f90c
ddontula3 <111503786+ddontula3@users.noreply.github.com>
2023-03-23 09:39:53 +0530

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,19 @@
+package firstproject;
+
+public class Assignment1 {
+	
+		public static void main(String[] args) {
+			// Implement your code here 
+	      int n1=5;
+	      int n2=5;
+	      if(n1==n2){
+	      System.out.println(n1+n2);
+	      }
+	      else	{
+	      System.out.println(2*(n1+n2));
+	      }
+	}
+	}
+
+
+
